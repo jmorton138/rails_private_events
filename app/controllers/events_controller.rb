2f404b6b799eb2d16bf1402957d6d46257b3be40
jeff <jmorton138@gmail.com>
2021-10-26 11:45:11 +0100

    User and event show pages list appropriate data. Create User controller and show view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,7 +5,26 @@ class EventsController < ApplicationController
     end
 
     def show
-        @user = User.find(params[:id])
-        @events = @user.created_events
+        @event = Event.find(params[:id])
+    end
+
+    def new
+        @event = Event.new
+    end
+
+    def create
+        @event = current_user.created_events.build(event_params)
+
+        if @event.save
+            redirect_to events_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def event_params
+        params.require(:event).permit(:event_date)
     end
 end
