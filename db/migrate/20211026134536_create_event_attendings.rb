9d6bb133c14669f04c668e10890e95e8593a417d
jeff <jmorton138@gmail.com>
2021-10-26 15:48:55 +0100

    Migrations and associations for attendees to events

--- db/migrate/20211026134536_create_event_attendings.rb
+++ db/migrate/20211026134536_create_event_attendings.rb
@@ -1,6 +1,8 @@
 class CreateEventAttendings < ActiveRecord::Migration[6.1]
   def change
     create_table :event_attendings do |t|
+      t.belongs_to :event_attendee
+      t.belongs_to :attended_event
 
       t.timestamps
     end
